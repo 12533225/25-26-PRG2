a5306ab74deed6942dae87bedb66f170168e4678
Maikoledu1303 <maikol.toyo@alumnos.uneatlantico.es>
2026-02-17 13:32:08 +0100

    feature: handle user input

--- entregas/toyoMaikol/src/Buscaminas.java
+++ entregas/toyoMaikol/src/Buscaminas.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Buscaminas {
     private static final int FILAS = 5;
@@ -6,6 +7,7 @@ public class Buscaminas {
     private static final char CELDA_OCULTA = '-';
     private static final int TOTAL_MINAS = 5;
     private boolean[][] mapaMinas;
+    private Scanner scanner = new Scanner(System.in);
 
     private char[][] tableroVisual;
 
@@ -48,13 +50,43 @@ public class Buscaminas {
     private void imprimirTablero() {
         System.out.println("================");
         System.out.print("  ");
+        for (int j = 1; j <= COLUMNAS; j++) { 
+            System.out.print(j + " ");
+        }
+        System.out.println();
 
         for (int i = 0; i < FILAS; i++) {
             System.out.print((i + 1) + " "); 
             for (int j = 0; j < COLUMNAS; j++) {
                 System.out.print(tableroVisual[i][j] + " ");
             }
+            System.out.println();
         }
         System.out.println("================");
+    }
+
+    private void realizarJugada() {
+        int fila = leerNumero("Fila (X)", FILAS);
+        int col = leerNumero("Columna (Y)", COLUMNAS);
+    
+        procesarCelda(fila - 1, col - 1); 
+    }
+
+    private int leerNumero(String dato, int max) {
+        int valor = -1;
+        while (true) {
+            System.out.println("Ingrese " + dato + " (1-" + max + "):");
+            if (scanner.hasNextInt()) {
+                valor = scanner.nextInt();
+                if (valor >= 1 && valor <= max) return valor;
+            } else {
+                scanner.next();
+            }
+            System.out.println("Error: Número inválido.");
+        }
+    }
+
+    private void procesarCelda(int f, int c) {
+        System.out.println("Analizando celda: " + f + "," + c);
     }
 }
